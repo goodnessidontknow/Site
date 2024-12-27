1429a531ff48c1ee13091bdadb85e534d3365659
John Slater <49700385+goodnessidontknow@users.noreply.github.com>
2024-12-27 13:29:07 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-$('#one').t(' ______         _____ _________ ______  ______\n|______ |      |_____|    |    |______ |_____/\n______| |_____ |     |    |    |______ |    \\_',
-    {speed:15,beep:true,caret:true,blink:false,fin:()=>{
-    $('.t-caret').remove();
-    $('#two').t('        ______\n       /     /\\\n      /     /  \\\n     /_____/----\\_    (\n    "     "          ).\n   _ ___          o (:\') o\n  (@))_))        o ~/~~\\~ o\n                   o  o  o',
-    {speed:0.1,beep:true,caret:true,blink:false, fin:()=>{
-        $('.t-caret').remove();
-        $('#three').t('slater26@purdue.edu\ngithub: goodnessidontknow\nlinkedin: /in/johnhenryslater\ninstagram: ___slater\n\n', {speed:15,beep:true,caret:true,blink:false,fin:()=>{
-            setTimeout({}, 500)
-            selectionMain();
-        }})
-    }})
-}})
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+        window.location.href = 'https://www.youtube.com';
+    }
+});
